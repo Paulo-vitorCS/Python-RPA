2fcd9d33c1932a4b09106919f159a947801906fc
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2022-12-01 14:31:34 -0300

    Opening Firefox and querying the dollar value

--- Robo01/main.py
+++ Robo01/main.py
@@ -3,17 +3,42 @@
 import pyautogui
 
 
-pyautogui.sleep(4)  # Faz um sleep para mudarmos a posição do mouse
+pyautogui.sleep(4)  # Aguarda um determinado tempo
 print(pyautogui.position())  # Imprime a posição (x,y) do mouse
 
 pyautogui.moveTo(x=1399, y=13)  # Move o cursor do mouse até a posição imposta
 pyautogui.click(x=1399, y=13)  # Click na posição desejada
-
 pyautogui.sleep(3)
 
-pyautogui.typewrite('Calculadora')
-
+pyautogui.typewrite('Firefox')
 pyautogui.sleep(2)
 
 pyautogui.moveTo(x=2354, y=203)
 pyautogui.click(x=2354, y=203)
+pyautogui.sleep(6)
+
+pyautogui.typewrite('https://www.google.com')
+pyautogui.sleep(2)
+
+pyautogui.press('enter')
+pyautogui.sleep(6)
+
+pyautogui.typewrite('Valor do dolar')
+pyautogui.sleep(2)
+pyautogui.press('enter')
+pyautogui.sleep(6)
+
+# pyautogui.sleep(4)  # Aguarda um determinado tempo
+# print(pyautogui.position())  # Imprime a posição (x,y) do mouse
+#
+# pyautogui.moveTo(x=1399, y=13)  # Move o cursor do mouse até a posição imposta
+# pyautogui.click(x=1399, y=13)  # Click na posição desejada
+#
+# pyautogui.sleep(3)
+#
+# pyautogui.typewrite('Calculadora')
+#
+# pyautogui.sleep(2)
+#
+# pyautogui.moveTo(x=2354, y=203)
+# pyautogui.click(x=2354, y=203)
