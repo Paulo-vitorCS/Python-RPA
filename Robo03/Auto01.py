065197f1db127a1b0406875b9459332e1d9022b0
Paulo Vitor Costa Silva <65197091+Paulo-vitorCS@users.noreply.github.com>
2023-01-09 10:53:33 -0300

    Saving adress data in a spreadsheet

--- Robo03/Auto01.py
+++ Robo03/Auto01.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import pyautogui
+from openpyxl import Workbook  # Bibliotecas para trabalhar com planilhas
+import os
 
 
 browser = webdriver.Firefox()
@@ -25,3 +26,34 @@ print(rua, bairro, uf, cep, sep='\n')
 
 pyautogui.sleep(2)
 browser.close()
+
+# ---------------------------------------------------------------
+# Salvando o endereço em uma planilha
+
+caminho_arquivo = '/home/paulo/PycharmProjects/Python-RPA/Robo03/pequisaEndereco.xlsx'
+
+# Usando o Workbook nesse formato de código, a planilha é sempre recriada!
+wb = Workbook()  # Classe para começar a trabalhar com a planilha
+
+planilha = wb.active  # Ativando o uso da planilha
+planilha.title = 'Dados'
+
+planilha['A1'] = 'Endereço'
+planilha['B1'] = 'Bairro'
+planilha['C1'] = 'Cidade'
+planilha['D1'] = 'CEP'
+
+linha = len(planilha['A']) + 1
+colunaA = 'A' + str(linha)
+colunaB = 'B' + str(linha)
+colunaC = 'C' + str(linha)
+colunaD = 'D' + str(linha)
+
+planilha[colunaA] = rua
+planilha[colunaB] = bairro
+planilha[colunaC] = uf
+planilha[colunaD] = cep
+
+wb.save(filename=caminho_arquivo)
+
+os.system('xdg-open ' + caminho_arquivo)  # Usado para abrir a plan. no linux | Windows: os.startfile(caminha_arquivo)
